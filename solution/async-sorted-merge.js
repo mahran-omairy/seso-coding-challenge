4fcb6b37d0deee2f3d627d9d733f15c065e228a9
Mahran Omairy <mahranomairy90@gmail.com>
2024-10-10 17:01:13 -0400

    challange solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -3,7 +3,41 @@
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+  return new Promise(async (resolve, reject) => {
+    try {
+      const logEntries = [];
+
+      for (let i = 0; i < logSources.length; i++) {
+        const log = await logSources[i].popAsync();
+        if (log) {
+          logEntries.push({ log, index: i });
+        }
+      }
+
+      logEntries.sort((a, b) => a.log.date - b.log.date);
+
+      while (logEntries.length > 0) {
+        const { log, index } = logEntries.shift();
+
+        printer.print(log);
+
+        const nextLog = await logSources[index].popAsync();
+
+        if (nextLog) {
+          let i = 0;
+
+          while (
+            i < logEntries.length &&
+            logEntries[i].log.date < nextLog.date
+          ) {
+            i++;
+          }
+          logEntries.splice(i, 0, { log: nextLog, index });
+        }
+      }
+      resolve(printer.done());
+    } catch (err) {
+      reject(err);
+    }
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,32 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const logEntries = [];
+
+  for (let i = 0; i < logSources.length; i++) {
+    const log = logSources[i].pop();
+    if (log) {
+      logEntries.push({ log, index: i });
+    }
+  }
+
+  logEntries.sort((a, b) => a.log.date - b.log.date);
+
+  while (logEntries.length > 0) {
+    const { log, index } = logEntries.shift();
+
+    printer.print(log);
+
+    const nextLog = logSources[index].pop();
+
+    if (nextLog) {
+      let i = 0;
+
+      while (i < logEntries.length && logEntries[i].log.date < nextLog.date) {
+        i++;
+      }
+      logEntries.splice(i, 0, { log: nextLog, index });
+    }
+  }
+  printer.done();
 };
